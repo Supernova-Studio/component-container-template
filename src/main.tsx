76d97eeec472a58797c0862ddf046fce11d8f656
Bartosz Adamczyk <bartoszadamczykk@gmail.com>
2026-04-02 15:22:12 +0200

    feat: update package and integrate prototyping tooling

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./app.tsx";
 import { SnPrototype } from "../supernova/helpers/snPrototype";
+import "@supernovaio/prototyping-tooling/client"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
